9892855e0739ef58d4a489c77a913e6e7a627898
followthewhiterabbit <3tonishere@gmail.com>
2020-02-26 20:50:51 +0300

    Add Header and Footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Header from '../components/header'
+import Footer from '../components/footer'
+
 const IndexPage = () => {
   return (
     <div>
+      <Header />
       <h1>Hello.</h1>
       <h1>I'm Vadim, a full-stack developer living in beautiful Vitebsk.</h1>
       <p>
         Need a developer? <Link to="/contact">Contact me.</Link>
       </p>
+      <Footer />
     </div>
   )
 }
