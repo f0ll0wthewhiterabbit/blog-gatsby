03e994193fed9ca9b4fe3dc0ee9d0d571136cf8f
followthewhiterabbit <3tonishere@gmail.com>
2020-02-26 19:05:24 +0300

    Add links between pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,13 @@ const ContactPage = () => {
   return (
     <div>
       <h1>Contact</h1>
-      <p>The best way to reach me is to call!</p>
+      <p>
+        The best way to reach me is to write via{' '}
+        <a href="https://vk.com/vad1m88" target="_blank">
+          VK
+        </a>
+        !
+      </p>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello.</h1>
       <h1>I'm Vadim, a full-stack developer living in beautiful Vitebsk.</h1>
+      <p>
+        Need a developer? <Link to="/contact">Contact me.</Link>
+      </p>
     </div>
   )
 }
