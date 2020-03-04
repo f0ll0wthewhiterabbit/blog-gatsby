fb6a84d739c9cf18f95f6b26da2d5b9eba5b0001
followthewhiterabbit <3tonishere@gmail.com>
2020-03-04 19:11:31 +0300

    Implement rendering posts from Contentful

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,16 +7,13 @@ import blogStyles from './blog.module.scss'
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
+            id
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
           }
         }
       }
@@ -27,11 +24,11 @@ const BlogPage = () => {
     <Layout>
       <h1>Blog</h1>
       <ol className={blogStyles.posts}>
-        {data.allMarkdownRemark.edges.map(edge => (
+        {data.allContentfulBlogPost.edges.map(edge => (
           <li className={blogStyles.post} key={edge.node.id}>
-            <Link to={`/blog/${edge.node.fields.slug}`}>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
+            <Link to={`/blog/${edge.node.slug}`}>
+              <h2>{edge.node.title}</h2>
+              <p>{edge.node.publishedDate}</p>
             </Link>
           </li>
         ))}
